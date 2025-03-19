3c39d7de7934512f6fcbeb56c518dbb32b56641d
hirata kyouko <tonkinese0chantilly.2615@gmail.com>
2025-03-19 23:26:43 +0900

    進捗状況 レスポンシブ対応
    追加 ヘッダー ハンバーガーボタン,
    
    ログイン画面 会員登録画面 メール認証画面 navエリア非表示
    
    仕掛 ヘッダー 表示崩れ修正, 商品購入画面

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -15,23 +15,59 @@
     <header class="header">
         <div class="header-inner">
             <div class="header-utilities">
-                <div class="a">
-                    <img src="{{ asset('storage/images/logo.svg') }}" width="300" height="80">
-                    <form class="form-search" action="/search" method="get">
-                        @csrf
-                        <input type="text" class="search-txt" name="keyword" placeholder="なにをお探しですか?">
-                        <!-- マイリスト選択時に検索→マイリストのまま結果表示 -->
-                        @if(Request::is('/'))
-                        @if ($data == null)
-                        <input type="hidden" name="tab" value="">
-                        @elseif ($data == 'mylist')
-                        <input type="hidden" name="tab" value="mylist">
-                        @endif
-                        @endif
-                        <button class="search-btn">検索</button>
-                    </form>
+                <img src="{{ asset('storage/images/logo.svg') }}" width="300" height="80">
+                <form class="form-search" action="/search" method="get">
+                    @csrf
+                    <input type="text" class="search-txt" name="keyword" placeholder="なにをお探しですか?">
+                    <!-- マイリスト選択時に検索→マイリストのまま結果表示 -->
+                    @if(Request::is('/'))
+                    @if ($data == null)
+                    <input type="hidden" name="tab" value="">
+                    @elseif ($data == 'mylist')
+                    <input type="hidden" name="tab" value="mylist">
+                    @endif
+                    @endif
+                    <button class="search-btn">検索</button>
+                </form>
+                <div class="hamburger" id="hamburger">
+                    <div class="icon">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
                 </div>
-                <nav class="b">
+                <nav class="sm">
+                    <ul class="nav-items">
+                        @if (Auth::check())
+                        <li class="nav-item">
+                            <form action="/logout" method="post">
+                                @csrf
+                                <button class="nav-btn">ログアウト</button>
+                            </form>
+                        </li>
+                        @else
+                        <li class="nav-item">
+                            <form action="/login" method="get">
+                                @csrf
+                                <button class="nav-btn" href="/login">ログイン</button>
+                            </form>
+                        </li>
+                        @endif
+                        <li class="nav-item">
+                            <form action="/mypage" method="post">
+                                @csrf
+                                <button class="nav-btn">マイページ</button>
+                            </form>
+                        </li>
+                        <li class="nav-item">
+                            <form action="/sell" method="get">
+                                @csrf
+                                <button class="listing-btn">出品</button>
+                            </form>
+                        </li>
+                    </ul>
+                </nav>
+                <nav class="pc">
                     <ul class="nav-items">
                         @if (Auth::check())
                         <li class="nav-item">
@@ -65,6 +101,13 @@
             </div>
         </div>
     </header>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script>
+        $('#hamburger').on('click', function() {
+            $('.icon').toggleClass('close');
+            $('.sm').slideToggle();
+        });
+    </script>
     <main>
         @yield('content')
     </main>
